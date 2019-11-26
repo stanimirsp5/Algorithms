746e84320f801ed864ac3dc145f9b2f6cb99de09
stanimirsp5 <stanimir.sp5@gmail.com>
2019-11-26 19:35:59 +0200

    quick sort complete

--- src/sorting/quickSort/Main.java
+++ src/sorting/quickSort/Main.java
@@ -3,7 +3,8 @@ package sorting.quickSort;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Comparable[] nums = {5,6,4,7,2,1,3,8,9};
+        Comparable[] nums = {5,9,7,1,2,4,3,8,6};
+        Comparable[] orderedNums = {0,1,2,3,4,5,6,7,8,9};
         int[] numsNoComparable = {10,6,4,7,2,1,3,8,9};
         int[] numsNoComparable2 = {5,6,4,7,2,1,3,8,9};
         Comparable[] items = {"z","y","h","i","a","w"};
@@ -11,7 +12,7 @@ public class Main {
 
         SortingPosition sp = new SortingPosition();
         QuickSort qs = new QuickSort();
-        sp.position(nums);
+        sp.sort(orderedNums);
         //qs.sort(numsNoComparable2);
     }
 }
--- src/sorting/quickSort/SortingPosition.java
+++ src/sorting/quickSort/SortingPosition.java
@@ -1,25 +1,38 @@
 package sorting.quickSort;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Random;
 
 public class SortingPosition {
-
-    public static void position(Comparable[] items){
-        //{5,6,4,7,2,1,3,8,9};
-        Comparable k = items[0];
-        int i = 0, j = items.length; // i searches for greater than pivot, j for smaller
-        while (i <= j){ // until i ang j crosses
-            while(less(items[++i], k))
-            while(less(k, items[--j]))
-            if(i >= j) break;
-            exchange(items, i,j);
-        }
+    public void sort(Comparable[] items){
+        shuffle(items);
+        System.out.println(Arrays.toString(items));
+        sort(items, 0, items.length-1);
         System.out.println(Arrays.toString(items));
     }
 
+    private void sort(Comparable[] items,int lo, int hi){
+        if(lo >= hi) return;
+        int pivot = position(items, lo,hi);
+        sort(items, lo ,pivot-1);
+        sort(items, pivot +1 ,hi);
+    }
+
+    public static int position(Comparable[] items, int lo, int hi){
+        Comparable k = items[lo];
+        int i = lo, j = hi+1; // i searches for greater than pivot, j for smaller
+        while (true){ // until i ang j crosses
+            while(less(items[++i], k));
+            while(less(k, items[--j]));
+            if(i >= j) {
+                exchange(items, lo,j);
+                break;
+            }
+            exchange(items, i,j);
+        }
+        return j;
+    }
+
     private static boolean less(Comparable a, Comparable b){
-        // return a.compareTo(b) < 0;
        return a.compareTo(b) < 0; // a > b => 1, a = b => 0, a < b => -1
     }
 
@@ -27,6 +40,15 @@ public class SortingPosition {
         Comparable t = items[i];
         items[i] = items[j];
         items[j] = t;
+    }
+
+    private static void shuffle(Comparable[] items){
+        Random rand = new Random();
+        int n = items.length;
+        for (int i = 0; i < n; i++) {
+            int r = rand.nextInt(n);
+            exchange(items, r,i);
+        }
     }
 
 }
