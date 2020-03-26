da3350ac5bfdb00da71d2aaea71655fc3f13d485
stanimirsp5 <stanimir.sp5@gmail.com>
2020-03-26 17:56:58 +0200

    graph input insert

--- src/graphs/Main.java
+++ src/graphs/Main.java
@@ -6,12 +6,17 @@ package graphs;
 import searching.RedBlackBST.StdOut;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 //import graphs.example.In;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         //String pathName = "C:\\Users\\stanimir.petrov\\Google Drive\\Algorithms\\Java\\src\\sources\\teenyWeenyG.txt";
+        //List list = new ArrayList();
+
         String pathName = "C:\\Users\\stanimir.petrov\\Google Drive\\Algorithms\\Java\\src\\sources\\teenyWeenyG.txt";
         In in = new In(pathName);
         Graph g = new Graph(in);
@@ -24,3 +29,15 @@ public class Main {
         g.printG();
     }
 }
+
+
+
+//        List<Integer> llist[] = new LinkedList[10];
+//        llist[0] = new LinkedList<>();
+//        llist[0].add(5);
+//        llist[0].add(13);
+//        llist[0].add(1);
+//
+//        for ( int i: llist[0]) {
+//            System.out.println(i);
+//        }
