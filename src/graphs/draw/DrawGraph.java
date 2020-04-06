ccbc8c37f2cd7361d6647579da576b800ea968a5
stanimirsp5 <stanimir.sp5@gmail.com>
2020-04-06 19:30:00 +0300

    draw graph, connect edges

--- src/graphs/draw/DrawGraph.java
+++ src/graphs/draw/DrawGraph.java
@@ -4,30 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
 
-public class DrawGraph extends JComponent {
+public class DrawGraph extends JPanel {
+
     @Override
-    public void paint(Graphics g) {
-        // Draw a simple line using the Graphics2D draw() method.
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(2f));
-        g2.setColor(Color.RED);
-        g2.draw(new Line2D.Double(50, 150, 250, 350));
-        g2.setColor(Color.GREEN);
-        g2.draw(new Line2D.Double(250, 350, 350, 250));
-        g2.setColor(Color.BLUE);
-        g2.draw(new Line2D.Double(350, 250, 150, 50));
-        g2.setColor(Color.YELLOW);
-        g2.draw(new Line2D.Double(150, 50, 50, 150));
-        g2.setColor(Color.BLACK);
-        g2.draw(new Line2D.Double(0, 0, 400, 400));
+    public void paintComponent(Graphics g) {
+        // Draw Tree Here
+        g.drawOval(5, 5, 10, 10);
+        g.drawOval(44, 33, 10, 10);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Draw Line");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new DrawGraph());
-        frame.pack();
-        frame.setSize(new Dimension(420, 440));
-        frame.setVisible(true);
+        JFrame jFrame = new JFrame();
+        jFrame.add(new DrawGraph());
+        jFrame.setSize(500, 500);
+        jFrame.setVisible(true);
     }
+
 }
+
+//public class DrawGraph extends JComponent {
+//    @Override
+//    public void paint(Graphics g) {
+//        // Draw a simple line using the Graphics2D draw() method.
+//        Graphics2D g2 = (Graphics2D) g;
+//        g2.setStroke(new BasicStroke(2f));
+//        g2.setColor(Color.RED);
+//        g2.draw(new Line2D.Double(50, 150, 250, 350));
+//        g2.setColor(Color.GREEN);
+//        g2.draw(new Line2D.Double(250, 350, 350, 250));
+//        g2.setColor(Color.BLUE);
+//        g2.draw(new Line2D.Double(350, 250, 150, 50));
+//        g2.setColor(Color.YELLOW);
+//        g2.draw(new Line2D.Double(150, 50, 50, 150));
+//        g2.setColor(Color.BLACK);
+//        g2.draw(new Line2D.Double(0, 0, 400, 400));
+//    }
+//
+//    public static void main(String[] args) {
+//        JFrame frame = new JFrame("Draw Line");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.getContentPane().add(new DrawGraph());
+//        frame.pack();
+//        frame.setSize(new Dimension(420, 440));
+//        frame.setVisible(true);
+//    }
+//}
--- src/graphs/draw/Java2D.java
+++ src/graphs/draw/Java2D.java
@@ -3,15 +3,15 @@ package graphs.draw;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.Random;
 
 public class Java2D extends JPanel{
     @Override
     public void paint(Graphics g){
 //        drawShape(g);
-//        drawPolyline(g);
+        drawPolyline(g);
 //        drawLine(g);
-        roundRectangle(g);
-
+//        roundRectangle(g);
     }
 //    public void drawLine(Graphics g){
 //        Graphics2D g2 = (Graphics2D) g;
@@ -28,21 +28,79 @@ public class Java2D extends JPanel{
 //
 //        g2.draw(polyline);
 //    }
-    public void drawPolyline(Graphics g){
+    public void drawVertex(Graphics g, int[] verticesCoordinates, int i){
+        Random rand = new Random();
+        int x = rand.nextInt(600);
+        int y = rand.nextInt(600);
+        verticesCoordinates[i] = x;
+        verticesCoordinates[i+1] = y;
+        // https://stackoverflow.com/questions/26785759/javascript-like-object-data-type-in-java
+//       obj = 1 { x:
+//                y:
+//            }
+//        Object myobj = new Object() {
+//           int success = 1,
+//           int success2 = 2,
+//        }
+       g.drawOval(x, y, 10, 10);
+    } 
+    public void drawEdges(Graphics g, int[] verticesCoordinates, int i){
         Graphics2D g2 = (Graphics2D) g;
-        int x2Points[] = {0, 100, 0, 100};
-        int y2Points[] = {0, 50, 50, 0};
+        int x1 = verticesCoordinates[i];
+        int y1 = verticesCoordinates[i];
+        g2.draw(new Line2D.Double(x1, y1, 20, 20));
+    }
+    public void drawPolyline(Graphics g){
+        int[][] graph = {{0}, {1,2}, {1},{0,4},{2},{0},{3},{4},{4},{3,1}};
+        
+        int numberOfVertices = graph.length;
+        int[] verticesCoordinates = new int[]{numberOfVertices};
 
-        GeneralPath polyline =
-                new GeneralPath(GeneralPath.WIND_EVEN_ODD, x2Points.length);
+        for (int i = 0; i < numberOfVertices; i++) {
+            drawVertex(g,verticesCoordinates, i);
+        }
+        
+        for (int i = 0; i < numberOfVertices; i++) {
+            for (int j = 1; j < graph[i].length; j+=1) {
+                drawEdges(g,verticesCoordinates, i);
+            }
+        }
 
-        polyline.moveTo (x2Points[0], y2Points[0]);
 
-        for (int index = 1; index < x2Points.length; index++) {
-            polyline.lineTo(x2Points[index], y2Points[index]);
-        };
 
-        g2.draw(polyline);
+
+
+
+
+
+
+
+
+
+//        int x2Points[] = {20, 50, 300, 150, 200};
+//        int y2Points[] = {20, 20, 100, 150, 200};
+//        int V = 5;
+//        g.drawOval(20, 20, 10, 10); //0
+//        g.drawOval(50, 20, 10, 10); //1
+//        g.drawOval(70, 50, 10, 10); //4
+//        g.drawOval(20, 80, 10, 10); //2
+//        g.drawOval(50, 80, 10, 10); //3
+//
+//        g2.draw(new Line2D.Double(20, 20, 50, 20)); // 0 -1
+//        g2.draw(new Line2D.Double(20, 20, 20, 80)); // 0 -2
+//        g2.draw(new Line2D.Double(50, 20, 70, 50)); // 1 -4
+//        g2.draw(new Line2D.Double(50, 80, 70, 50)); // 3 -4
+//        g2.draw(new Line2D.Double(50, 80, 20, 20)); // 3 -0
+
+
+//        GeneralPath polyline =
+//                new GeneralPath(GeneralPath.WIND_EVEN_ODD, x2Points.length);
+//
+//        polyline.moveTo(x2Points[0], y2Points[0]);
+//        for (int index = 1; index < x2Points.length; index++) {
+//            polyline.lineTo(x2Points[index], y2Points[index]);
+//        };
+//        g2.draw(polyline);
     }
     public void roundRectangle(Graphics g){
         Graphics2D g2 = (Graphics2D) g;
@@ -60,7 +118,7 @@ public class Java2D extends JPanel{
                 10, 10));
     }
 
-public void drawShape(Graphics g) {
+    public void drawShape(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
     // Draw Hello World String
     g2.drawString("Hello W11orld!", 50, 100);
