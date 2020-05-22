81ea0a76229d345e7d081bb8912e668ba0ef3658
stanimirsp5 <stanimir.sp5@gmail.com>
2020-05-22 08:59:05 +0300

    cc print

--- src/graphs/CC/CC.java
+++ src/graphs/CC/CC.java
@@ -1,6 +1,11 @@
 package graphs.CC;
 
 import graphs.Graph;
+import graphs.In;
+
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CC {
     private boolean[] marked;
@@ -38,6 +43,32 @@ public class CC {
 
     public int count(){
         return count;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String pathName = "C:\\Users\\stanimir.petrov\\Google Drive\\Algorithms\\Java\\src\\sources\\twoGraphsG.txt";//Mac //twoGraphsG,teenyWeenyG,tinyG,mediumG,verySmallG
+        //String pathName = "/Users/stanimir/Projects/Algorithms/src/sources/twoGraphsG.txt";//Mac //teenyWeenyG,tinyG,mediumG,verySmallG
+        In in = new In(pathName);
+        Graph G = new Graph(in);
+        CC cc = new CC(G);
+
+        int n = cc.count();
+        System.out.println(n + " components");
+
+        Queue<Integer>[] components;
+        components = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            components[i] = new LinkedList<Integer>();
+        }
+        for (int v = 0; v < G.V(); v++) {
+            components[cc.id(v)].remove(v);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int v : components[i]) {
+                System.out.print(v +" ");
+            }
+            System.out.println();
+        }
     }
 
 }
