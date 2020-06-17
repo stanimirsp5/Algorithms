6f3ca16242e4fe47042035f20064be8fa812f830
Stan <stanimir.sp5@gmail.com>
2020-06-17 23:26:36 +0300

    pass args to main

--- src/scratches.java
+++ src/scratches.java
@@ -1,6 +1,8 @@
 public class scratches {
 //$ java MyProg -f file.txt
 // https://stackoverflow.com/questions/890966/what-is-string-args-parameter-in-main-method-java
+    // javac scratches.java - compile code
+    // java scratches -f file.txt pass arguments
     public static void main(String[] args) {
         System.out.println(args.length);
 //        System.out.println(foo(2));
